fdf6c0e5a298f6e8b4858af95bed983c1a3d0c8a
Nguyễn Hoàng Anh <nguyenhoanganh@luvina.net>
2022-05-30 18:40:06 +0700

    create ecommerce page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,16 +9,105 @@ import { Cart, Chat, Notification, UserProfile } from ".";
 import { useStateContext } from "../contexts/ContextProvider";
 import avatar from "../data/avatar.jpg";
 
-const NavButton = ({title, customFunc}) => {
-
-}
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
+  return (
+    <TooltipComponent content={title} position="BottomCenter">
+      <button
+        type="button"
+        onClick={customFunc}
+        style={{ color }}
+        className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+      >
+        <span
+          style={{ background: dotColor }}
+          className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        />
+          {icon}
+      </button>
+    </TooltipComponent>
+  );
+};
 
 const Navbar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const {
+    activeMenu,
+    setactiveMenu,
+    isClicked,
+    setIsClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext();
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener('reszie', handleResize)
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setactiveMenu(false);
+    } else {
+      setactiveMenu(true);
+    }
+  }, [screenSize])
 
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
-      
+      <NavButton
+        title="Menu"
+        customFunc={() => setactiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunc={() => handleClick("Cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+
+        <NavButton
+          title="Chat"
+          customFunc={() => handleClick("Chat")}
+          color="blue"
+          dotColor="#03C9D7"
+          icon={<BsChatLeft />}
+        />
+
+        <NavButton
+          title="Notification"
+          customFunc={() => handleClick("Notification")}
+          color="blue"
+          dotColor="#03C9D7"
+          icon={<RiNotification3Line />}
+        />
+
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("Profile")}
+          >
+            <img className="rounded-full w-8 h-8" src={avatar} />
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.Notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
+      </div>
     </div>
   );
 };
