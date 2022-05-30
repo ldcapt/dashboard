6d30a6ecf0a8a60a3a492bd1976a786c888dacde
Nguyễn Hoàng Anh <nguyenhoanganh@luvina.net>
2022-05-30 21:25:21 +0700

    update page order, employee, customer, calendar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-const Header = () => {
+const Header = ({category, title }) => {
   return (
-    <div></div>
+    <div className="mb-10">
+      <p className="text-gray-10">
+        {category}
+      </p>
+      <p className="text-3xl font-extrabold tracking-tight text-slate-900">
+        {title}
+      </p>
+    </div>
   );
 }
 
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -82,6 +82,36 @@ const Ecommerce = () => {
                 </p>
                 <p className="text-gray-500 mt-1">Budget</p>
               </div>
+              <div className="mt-8 ">
+                <p>
+                  <span className="text-3xl font-semibold">$48,438</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              <div className="mt-5 ">
+                <SparkLine 
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+              <div className="mt-10">
+                <Button 
+                  color="white"
+                  bgColor="blue"
+                  text="Download Report"
+                  borderRadius="10px" />
+              </div>
+            </div>
+            <div>
+              <Stacked 
+                width="320px"
+                height="360px"
+              />
             </div>
           </div>
         </div>
